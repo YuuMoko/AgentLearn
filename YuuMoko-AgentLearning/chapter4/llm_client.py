3d08a0257a533e495e02f8d490421d931208152a
YuuMoko <zyyang@streamax.com>
2026-05-21 22:01:42 +0800

    存档

---
+++ YuuMoko-AgentLearning/chapter4/llm_client.py
@@ -0,0 +1,7 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class HelloAgentsLLM:
+    def __init__(self, str = None, apiKey: str = None, baseUrl: str = None, timeout: int = None):
+
--- code/chapter4/llm_client.py
+++ code/chapter4/llm_client.py
@@ -1,10 +1,14 @@
 import os
+
+from hello_agents import HelloAgentsLLM
 from openai import OpenAI
 from dotenv import load_dotenv
 from typing import List, Dict
 
 # 加载 .env 文件中的环境变量
 load_dotenv()
+
+
 
 class HelloAgentsLLM:
     """
